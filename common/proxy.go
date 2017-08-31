d500d433e7e8a0ce4f621e17eb0b8bcd5204700b
crabkun <529493022@qq.com>
2017-08-31 12:31:21 +0800

    增加数据库配置项

--- common/proxy.go
+++ common/proxy.go
@@ -8,6 +8,9 @@ import (
 type S_config struct{
 	Debug bool
 	Proxy []S_proxy
+	DatabaseDriver string
+	DatabaseConnectionString string
+	NoAuth bool
 }
 type S_proxy struct{
 	Port string
@@ -20,9 +23,13 @@ type S_proxy struct{
 	Obscure string
 	ObscureParam string
 
+	//服务器所属组
+	Group string
+
 	//加密与伪装的接口
 	Ob obscure.Action `json:"-"`
 	E encryption.Action `json:"-"`
 	EReserved interface{} `json:"-"`
 	ObReserved interface{} `json:"-"`
+	Config S_config `json:"-"`
 }
