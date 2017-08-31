8ed97b6aa9bbb49a6d125a1def566388761e9814
crabkun <529493022@qq.com>
2017-08-31 12:31:03 +0800

    优化util

--- util/util.go
+++ util/util.go
@@ -3,6 +3,10 @@ package util
 import (
 	"time"
 	"math/rand"
+	"strings"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
 )
 
 func GetRandomString(strlen int) string{
@@ -15,4 +19,36 @@ func GetRandomString(strlen int) string{
 	}
 	return string(result)
 }
+func StrPadding(str string,count int,char string) string {
+	l:=count-len(str)
+	newstr:=str+strings.Repeat(char,l)
+	return newstr
+}
+func Gen16Md5Key(key string) ([]byte,error){
+	test := md5.New()
+	_,err:=test.Write([]byte(key))
+	if err!=nil{
+		return nil,err
+	}
+	return test.Sum(nil),nil
+}
+func Gen32Md5Key(key string) ([]byte,error){
+	test := md5.New()
+	_,err:=test.Write([]byte(key))
+	if err!=nil{
+		return nil,err
+	}
+	md5src:=test.Sum(nil)
+	md5dst:=make([]byte,32)
+	hex.Encode(md5dst,md5src)
+	return md5dst,nil
 
+}
+func GetDoubleMd5(str string) string {
+	test := md5.New()
+	test.Write([]byte(str))
+	doublemd5 := fmt.Sprintf("%x", test.Sum(nil))
+	test.Reset()
+	test.Write([]byte(doublemd5))
+	return fmt.Sprintf("%x", test.Sum(nil))
+}
