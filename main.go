b40b709f15cd35ee41dc5acc1b861f526139ba96
crabkun <529493022@qq.com>
2017-08-31 12:35:12 +0800

    增加数据库和免验证支持

--- main.go
+++ main.go
@@ -4,21 +4,32 @@ import (
 	"github.com/crabkun/DazeProxy/common"
 	"github.com/crabkun/DazeProxy/helper"
 	"github.com/crabkun/DazeProxy/server"
-	"time"
+	_ "github.com/crabkun/DazeProxy/database"
 	"log"
+	"github.com/crabkun/DazeProxy/database"
+	"sync"
+	"os"
 )
 
 var config common.S_config
+var wg sync.WaitGroup
 
 func main(){
-	log.Println("DazeProxy V3-201708301")
 	//加载配置
 	helper.LoadConfig(&config)
+	log.Println("DazeProxy V3-201708311")
+	//如果处于验证模式则连接数据库
+	if !config.NoAuth{
+		database.LoadDatabase(config.DatabaseDriver,config.DatabaseConnectionString)
+	}else{
+		log.Println("警告：服务器处于免验证模式！")
+	}
 	//启动代理实例
 	for _,s:=range config.Proxy{
-		go server.StartServer(s)
+		s.Config=config
+		wg.Add(1)
+		go server.StartServer(s,wg)
 	}
-	for{
-		time.Sleep(time.Second*10)
-	}
+	wg.Wait()
+	os.Exit(-1)
 }
