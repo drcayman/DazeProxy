e517755e5b35a88e66c4ba9b9b6c058cb1f42b9c
crabkun <529493022@qq.com>
2017-09-04 14:27:36 +0800

    增加日志输出到文件

--- helper/config.go
+++ helper/config.go
@@ -4,24 +4,25 @@ import (
 	"io/ioutil"
 	"encoding/json"
 	"os"
-	"fmt"
 	"github.com/crabkun/DazeProxy/common"
+	"log"
 )
 var IsDebug bool
-func LoadConfig(config *common.S_config){
+func LoadConfig(config *common.S_config,logflag bool){
 	buf,ReadErr:=ioutil.ReadFile("config.json")
 	if ReadErr!=nil{
-		fmt.Println("配置文件(config.json)读取错误："+ReadErr.Error())
+		log.Println("配置文件(config.json)读取错误："+ReadErr.Error())
 		os.Exit(-1)
 	}
 	JsonErr:=json.Unmarshal(buf,config)
 	if JsonErr!=nil{
-		fmt.Println("配置文件格式错误！请严格按照JSON格式来修改config.json(",JsonErr.Error(),")")
+		log.Println("配置文件格式错误！请严格按照JSON格式来修改config.json(",JsonErr.Error(),")")
 		os.Exit(-1)
 	}
-	fmt.Println("配置文件读取成功：")
-	fmt.Println("    调试模式：",config.Debug)
-	fmt.Printf("一共%d个代理服务单元\n",len(config.Proxy))
-	os.Stdout.Sync()
-	IsDebug=config.Debug
+	log.Println("配置文件读取成功：")
+	log.Println("    调试模式：",!logflag && config.Debug)
+	log.Printf("一共%d个代理服务单元\n",len(config.Proxy))
+	if !logflag{
+		IsDebug=config.Debug
+	}
 }
--- main.go
+++ main.go
@@ -4,22 +4,36 @@ import (
 	"github.com/crabkun/DazeProxy/common"
 	"github.com/crabkun/DazeProxy/helper"
 	"github.com/crabkun/DazeProxy/server"
-	_ "github.com/crabkun/DazeProxy/database"
 	"log"
 	"github.com/crabkun/DazeProxy/database"
 	"sync"
 	"os"
 	"math/rand"
 	"time"
+	"github.com/crabkun/go-args"
 )
 
 var config common.S_config
 var wg sync.WaitGroup
 
 func main(){
+	var logflag bool
 	rand.Seed(time.Now().UnixNano())
+	//处理启动参数
+	args:=go_args.ReadArgs()
+	if _,logflag=args.GetArg("-log");logflag{
+		logfile,err:=os.OpenFile("DazeProxy.log",os.O_CREATE|os.O_WRONLY, 0666)
+		if err!=nil{
+			log.Println("日志文件创建失败！原因：",err.Error())
+		}else{
+			helper.IsDebug=false
+			log.Println("进入日志模式，所有控制台输出已重定向到DazeProxy.log")
+			log.SetOutput(logfile)
+			log.Println("进入日志模式，已关闭调试模式")
+		}
+	}
 	//加载配置
-	helper.LoadConfig(&config)
+	helper.LoadConfig(&config,logflag)
 	log.Println("DazeProxy V3-201709031")
 	//如果处于验证模式则连接数据库
 	if !config.NoAuth{
